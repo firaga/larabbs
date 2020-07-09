4316778e4af7620b4204c10f6c10b966ffd3d3e9
vagrant <vagrant@homestead>
2020-07-09 12:12:01 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,17 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+
+    $update_at = $faker->dateTimeThisMonth();
+
+    $create_at = $faker->dateTimeThisMonth($update_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $create_at,
+        'updated_at' => $update_at,
     ];
 });
