842a4ee7d029fbdc492a163c373d4c9e74322dd0
vagrant <vagrant@homestead>
2020-07-09 23:50:37 +0000

    用户可以创建话题

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Builder;
+
 /**
  * App\Models\Topic
  *
@@ -44,7 +46,7 @@ use Illuminate\Database\Eloquent\Builder;
  */
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
     public function category()
     {
